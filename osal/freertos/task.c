fee45b8effd21ca50c1e4bc0f6f3c76c680dda43
Vládik Cegarra Mauriz <vladik.cegarra@uvigoaerotech.com>
2025-11-01 08:06:03 +0000

    Improved task create version

--- osal/freertos/task.c
+++ osal/freertos/task.c
@@ -4,21 +4,25 @@
 #include "core/types.h"
 #include "core/macros.h"
 
+
 void* SPP_OSAL_TaskCreate(void *p_function, const char *const task_name, 
                             const uint32_t stack_depth,void *const p_custom_data,
-                            spp_uint32_t priority, void* p_stack_buffer,
-                            void* p_task_buffer )
+                            spp_uint32_t priority, uint64_t stack_size)
 {
-    if (p_function == NULL || task_name == NULL || p_stack_buffer == NULL || p_task_buffer == NULL){
+    if (p_function == NULL || task_name == NULL){
         return NULL;
     }
+
+    StackType_t xStack[stack_size];
+    StaticTask_t xTaskBuffer;
+
     TaskHandle_t p_task = xTaskCreateStatic((TaskFunction_t) p_function, 
-                                                    task_name, 
-                                                    stack_depth, 
-                                                    p_custom_data, 
-                                                    (UBaseType_t) priority, 
-                                                    (StackType_t *) p_stack_buffer, 
-                                                    (StaticTask_t *) p_task_buffer);
+                                            task_name, 
+                                            stack_depth, 
+                                            p_custom_data, 
+                                            (UBaseType_t) priority, 
+                                            &xStack, 
+                                            &xTaskBuffer);
     if (p_task == NULL){
         return NULL;
     }
